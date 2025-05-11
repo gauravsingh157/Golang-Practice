d68b805ed4094d4e85d7c8e0704f34782249caaa
gauravsingh157 <gauravsinghsingh425@gmail.com>
2025-05-11 14:02:35 +0530

    `Update main.go: change STUDENT array assignments and add newline at end of file`

--- ARRAY/main.go
+++ ARRAY/main.go
@@ -5,10 +5,12 @@ import "fmt"
 func main() {
 	// Create a new map
 
-    var STUDENT [3]string
+	var STUDENT [3]string
 	STUDENT[0] = "John"
 	STUDENT[1] = "Mary"
-		
+	STUDENT[1] = "jon"
+	STUDENT[1] = "don"
+	STUDENT[1] = "marry"
 
 	fmt.Println(STUDENT)
 
