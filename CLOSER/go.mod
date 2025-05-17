c135e9cc79c726f659000e3138c52872d35a9d8c
gauravsingh157 <gauravsinghsingh425@gmail.com>
2025-05-17 14:25:32 +0530

    defer

---
+++ CLOSER/go.mod
@@ -0,0 +1,3 @@
+module Golang-Practice/CLOSER
+
+go 1.23.3
--- DB-CONNECTION/main.go
+++ DB-CONNECTION/main.go
@@ -9,30 +9,31 @@ import (
 	_ "github.com/lib/pq" // PostgreSQL driver
 )
 
-// DB is a global database connection variable
+
 var DB *sql.DB
 
-// ConnectDB initializes the PostgreSQL connection
 func  ConnectDB() {
 	var err error
 
-	// Get database URL from environment variables
-	dsn := os.Getenv("DATABASE_URL") // Example: "postgres://user:password@localhost:5432/dbname?sslmode=disable"
+	
+	dsn := os.Getenv("DATABASE_URL")
 
 	// Open PostgreSQL connection
 	DB, err = sql.Open("postgres", dsn)
 	if err != nil {
-		log.Fatal("❌ Failed to connect to database:", err)
+		log.Fatal(" Failed to connect to database:", err)
 	}
 
 	// Verify connection
 	err = DB.Ping()
 	if err != nil {
-		log.Fatal("❌ Database not responding:", err)
+		log.Fatal(" Database not responding:", err)
 	}
 
-		fmt.Println("✅ Successfully connected to PostgreSQL database!")
+		fmt.Println(" Successfully connected to PostgreSQL database!")
 	}
 func main(){
 	fmt.Println("DataBase Connetion")
+	ConnectDB().
+
 }
